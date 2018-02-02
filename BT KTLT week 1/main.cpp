a5c6dc3082ef19851046238197215d3936161998
LH Nam <lehainam38@gmail.com>
2018-02-02 17:16:14 +0700

    Initial Commit

---
+++ BT KTLT week 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BT KTLT week 1
+//
+//  Created by Nam 😋 on 2/2/18.
+//  Copyright © 2018 Nam 😋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
